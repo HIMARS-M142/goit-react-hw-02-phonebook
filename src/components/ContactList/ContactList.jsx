27af82e44a58dde7e465deabe8985181010e5c9e
HIMARS-M142 <maksympona@gmail.com>
2023-09-17 15:32:28 +0200

    upd

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,36 @@
+import { hover } from '@testing-library/user-event/dist/hover';
+import { styled } from 'styled-components';
+const ContactsStyled = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+const ButtonContact = styled.button`
+  margin-left: 50px;
+  background-color: antiquewhite;
+  border-radius: 5px;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+`;
+const ListStyled = styled.li`
+  align-items: center;
+  display: flex;
+`;
+export const ContactList = ({ contacts, onButtonDelete }) => {
+  return (
+    <ContactsStyled>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <ListStyled key={id}>
+            {name}: {number}
+            <ButtonContact onClick={() => onButtonDelete(id)}>
+              Delete
+            </ButtonContact>
+          </ListStyled>
+        );
+      })}
+    </ContactsStyled>
+  );
+};
